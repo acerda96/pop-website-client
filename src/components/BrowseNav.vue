3dab1cb600d7ae5f149d75e74fbb522987719459
Alex Cerda <acerdauk@gmail.com>
2020-10-31 22:48:46 +0000

    Fix browse nav styling

--- src/components/BrowseNav.vue
+++ src/components/BrowseNav.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="browse-nav">
-    <div class="search-container">
+    <div class="browse-nav__location">
       <button class="map-marker">
         <MapMarker />
       </button>
-      <input placeholder="City/postcode" class="input" />
+      <input placeholder="City/postcode" class="filter" />
     </div>
-    <select class="input dropdown" v-model="sortCriterion" @change="onChange">
+    <select class="filter dropdown" v-model="sortCriterion" @change="onChange">
       <option value disabled>Sort by...</option>
       <option value="0">Recently added</option>
       <option value="1">Price</option>
     </select>
-    <select class="input dropdown" v-model="type" @change="onChange">
+    <select class="filter dropdown" v-model="type" @change="onChange">
       <option value disabled>Filter by...</option>
       <option value="0">View All</option>
       <option value="1">Bottoms</option>
@@ -44,73 +44,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-@import "../styles/abstracts/_variables.scss";
-@import "vue-select/src/scss/vue-select.scss";
-
-$input-height: 30px;
-
-.browse-nav {
-  button {
-    border-radius: 5px;
-    border: none;
-    margin: 5px;
-    height: $input-height;
-  }
-}
-
-.map-marker {
-  background: none;
-  border: none;
-  color: white;
-  cursor: pointer;
-}
-
-.input {
-  padding: 7px;
-  margin: 10px;
-  width: 150px;
-  border-radius: 5px;
-  border: none;
-}
-
-.search-container {
-  display: flex;
-  align-items: center;
-  height: $input-height;
-}
-
-.browse-nav {
-  background-color: $sub-accent-color;
-  width: 100%;
-  padding: 10px;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  flex-wrap: wrap;
-}
-@media (max-width: 570px) {
-  .search-container {
-    margin-left: 90px;
-    margin-right: 90px;
-  }
-}
-
-@media (max-width: 400px) {
-  .browse-nav {
-    flex-direction: column;
-    flex-wrap: nowrap;
-    align-items: space-evenly;
-    justify-content: space-evenly;
-  }
-  .input {
-    padding: 4px;
-    width: 120px;
-  }
-  .dropdown {
-    width: 170px;
-    margin: 5px;
-  }
-}
-</style>
--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="browse">
     <BrowseNav @search="search" />
-    <p v-if="isItemsLoading">Loading items...</p>
-    <div v-if="!isItemsLoading" class="items-container">
-      <div class="item" v-for="item in items" :key="item._id">
-        <router-link :to="'/browse/' + item._id">
-          <div class="item-details">
-            {{ item.name }}
-            {{ "£" + item.unitPrice }}
-            <HeartOutline />
-            <HeartFilled />
-          </div>
-          <img
-            class="item-thumbnail"
-            v-bind:src="'data:image/jpeg;base64,' + item.images[0].buffer"
-          />
-        </router-link>
+    <div class="browse__body">
+      <Loader v-if="isLoading" />
+      <div v-if="!isLoading" class="browse__items">
+        <div class="browse__item" v-for="item in items" :key="item._id">
+          <router-link :to="'/browse/' + item._id">
+            <div class="browse__item-details">
+              {{ item.name }}
+              {{ "£" + item.unitPrice }}
+              <HeartOutline />
+              <HeartFilled />
+            </div>
+            <img
+              class="browse__item-thumbnail"
+              v-bind:src="'data:image/jpeg;base64,' + item.images[0].buffer"
+            />
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -26,13 +28,14 @@ import BrowseNav from "../components/BrowseNav";
 import HeartOutline from "vue-material-design-icons/HeartOutline.vue";
 import HeartFilled from "vue-material-design-icons/Heart.vue";
 import axios from "axios";
+import Loader from "../components/Loader.vue";
 
 export default {
   name: "Browse",
-  components: { BrowseNav, HeartOutline, HeartFilled },
+  components: { BrowseNav, HeartOutline, HeartFilled, Loader },
   data() {
     return {
-      isItemsLoading: true,
+      isLoading: true,
       individual: {},
       items: [],
     };
@@ -55,40 +58,20 @@ export default {
         .get("api/items")
         .then((res) => {
           this.items = res.data;
-          this.isItemsLoading = false;
+          this.isLoading = false;
         })
         .catch((err) => console.log(err));
     },
     search(sortCriterion, type) {
-      this.isItemsLoading = true;
+      this.isLoading = true;
       axios
         .get(`api/items?sortCriterion=${sortCriterion}&type=${type}`)
         .then((res) => {
           this.items = res.data;
-          this.isItemsLoading = false;
+          this.isLoading = false;
         })
         .catch((err) => console.log(err));
     },
   },
 };
 </script>
-
-<style lang="scss">
-@import "../styles/abstracts/_variables.scss";
-
-.browse {
-  background: white;
-  min-height: 500px;
-
-  p {
-    text-align: center;
-  }
-}
-
-.item-details {
-  display: flex;
-  align-items: center;
-  color: black;
-  justify-content: space-evenly;
-}
-</style>
