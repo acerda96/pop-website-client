d2b5f76714701e30bdc85a0254e75f170fa6f4bf
Alex Cerda <acerdauk@gmail.com>
2020-12-26 01:41:31 +0000

    Remove webpack plugin config

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
+// const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "./public"),
-  configureWebpack: {
-    plugins: [
-      new HTMLWebpackPlugin({
-        showErrors: true,
-        cache: true,
-        template: path.resolve(__dirname, "src/index.html"),
-      }),
-    ],
-  },
+  // configureWebpack: {
+  //   plugins: [
+  //     new HTMLWebpackPlugin({
+  //       showErrors: true,
+  //       cache: true,
+  //       template: path.resolve(__dirname, "src/index.html"),
+  //     }),
+  //   ],
+  // },
 };
